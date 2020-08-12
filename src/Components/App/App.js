33ed17d153d56ce0d1d05548d8fc6e4243b7f899
mgd34msu <mgd34msu@gmail.com>
2020-08-12 11:12:23 -0500

    playlist onNameChange and onSave methods added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -20,6 +20,8 @@ class App extends React.Component{
 
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track){
@@ -37,6 +39,15 @@ class App extends React.Component{
     this.setState({playlistTracks: tracks});
   }
 
+  updatePlaylistName(name){
+    this.setState({playlistName: name});
+  }
+
+  savePlaylist(){
+    const trackURIs = [];
+    this.state.playlistTracks.map(track => track.uri);
+  }
+
   render(){
     return(
       <div>
@@ -48,7 +59,9 @@ class App extends React.Component{
                             onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName}
                       playlistTracks={this.state.playlistTracks}
-                      onRemove={this.removeTrack} />
+                      onRemove={this.removeTrack}
+                      onNameChange={this.updatePlaylistName}
+                      onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
